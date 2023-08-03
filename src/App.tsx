4a9534dbfc31252cc7aa1648631574c1af1c76f3
Georg Bremer <github@dschoordsch.de>
2023-08-03 11:29:24 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import words from './words-de';
 
 function App() {
+  const [used, setUsed] = React.useState(new Set())
+  const [index, setIndex] = React.useState(Math.floor(Math.random() * words.length))
+
+  const randomize = () => {
+    const newUsed = new Set(used.add(index))
+    setUsed(newUsed)
+
+    let newIndex = Math.floor(Math.random() * words.length)
+    while (newUsed.has(newIndex)) {
+      newIndex = Math.floor(Math.random() * words.length)
+    }
+    setIndex(newIndex)
+  }
+
+  const onKeyDown = React.useCallback((e: React.KeyboardEvent) => {
+    if(e.keyCode == 32) {
+      randomize()
+    }
+  }, [])
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', onKeyDown as any)
+    return () => window.removeEventListener('keydown', onKeyDown as any)
+  }, [])
+
+  const gameOver = React.useMemo(() => {
+    return used.size === words.length
+  }, [used])
+
   return (
-    <div className="App">
+    <div className="App" onKeyDown={onKeyDown}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          {words[index]}
+        </h1>
+        {gameOver ?
+          <h2>Alle Wörter durch!</h2> :
+          <button onClick={randomize}><h2>Nächstes Wort</h2></button>
+        }
       </header>
     </div>
   );
